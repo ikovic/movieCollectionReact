11db4713987e128032a42d7b0288a210099c3cc6
Ivan <kovic.ivan@gmail.com>
2016-08-05 00:27:32 +0200

    Styling changes

--- src/app/components/sidebar/sidebar.js
+++ src/app/components/sidebar/sidebar.js
@@ -6,6 +6,7 @@ export default class Sidebar extends Component {
     render() {
         return (
             <aside id="sidebar">
+                <h4>Pick a collection: </h4>
                 <ul>
                     <li>Random Link</li>
                     <li>Random Link</li>
