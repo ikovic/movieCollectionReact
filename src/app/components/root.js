c4ea805446c42dc82f09d702d4e1f36183fad2fd
Ivan Kovic <kovic.ivan@gmail.com>
2016-08-04 16:19:47 +0200

    Work in progress

---
+++ src/app/components/root.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+export default class Root extends Component {
+    constructor() {
+        super();
+        console.log('test');
+    }
+
+    render() {
+        return (
+            <h1>Veliki test</h1>
+        );
+    }
+}
