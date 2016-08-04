1a49afbdd06aeb3226c747b83e99239bf9a106c2
Ivan <kovic.ivan@gmail.com>
2016-08-04 23:46:57 +0200

    Improved cards

--- src/app/components/movies/card/card.js
+++ src/app/components/movies/card/card.js
@@ -9,9 +9,9 @@ export default class Card extends Component {
                 <img
                     src="http://ia.media-imdb.com/images/M/MV5BMTU0ODk1MTIxM15BMl5BanBnXkFtZTgwNTk3MTc5ODE@._V1_SX300.jpg"/>
                 <div className="card-data">
-                    <h4>Star Trek</h4>
-                    <h4>2014.</h4>
-                    <h4>IMDb: 7.5/10</h4>
+                    <p className="title">Star Trek</p>
+                    <p className="year">2014.</p>
+                    <p className="rating">IMDb: 7.5/10</p>
                 </div>
 
             </article>
