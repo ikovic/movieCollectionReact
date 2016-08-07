2e8b00c4929f65ffcab1ed20e99c18b545eacb8e
Ivan <kovic.ivan@gmail.com>
2016-08-07 23:19:34 +0200

    Login stuff

--- src/app/components/header/header.js
+++ src/app/components/header/header.js
@@ -1,10 +1,19 @@
 import React, {Component} from 'react';
 import sessionStore from '../../stores/sessionStore';
 import sessionActions from '../../actions/sessionActions';
+import GoogleLogin from 'react-google-login';
 
 import './header.scss';
 
 export default class Header extends Component {
+
+    responseGoogle(googleUser) {
+        console.dir(googleUser);
+
+        // The ID token you need to pass to your backend:
+        var id_token = googleUser.getAuthResponse().id_token;
+        console.log("ID Token: " + id_token);
+    }
 
     onChange() {
         console.log('change');
@@ -24,7 +33,12 @@ export default class Header extends Component {
                 <img className="logo"
                      src={require('../../../../public/images/logo.png')}/>
                 <div className="account">
-                    <p onClick={() => sessionActions.signInUser()}>Sign in</p>
+                    <GoogleLogin
+                        clientId="103058587609-srk6qkhe6hegud2a23g4n29d34hj07fi.apps.googleusercontent.com"
+                        buttonText="Sign in With Google"
+                        cssClass="google-sign-in default"
+                        callback={(response) => this.responseGoogle(response)}
+                    />
                 </div>
             </header>
         )
