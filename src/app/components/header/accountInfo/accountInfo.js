0588c6a1ef27351e333ac1efbca5bf126a94eef8
Ivan <kovic.ivan@gmail.com>
2016-08-08 11:07:33 +0200

    Account info layout fixes

--- src/app/components/header/accountInfo/accountInfo.js
+++ src/app/components/header/accountInfo/accountInfo.js
@@ -9,14 +9,12 @@ export default class AccountInfo extends Component {
 
     }
 
-
     render() {
         var userInfo = this.props.user.getBasicProfile();
         return (
             <div id="accountInfo">
-                <span>{userInfo.getName()}</span>
-                <span onClick={() => sessionActions.signOutUser()}>Sign out</span>
-
+                <h3 className="username">{userInfo.getName()}</h3>
+                <span className="sign-out" onClick={() => sessionActions.signOutUser()}>Sign out</span>
             </div>)
     }
 }
